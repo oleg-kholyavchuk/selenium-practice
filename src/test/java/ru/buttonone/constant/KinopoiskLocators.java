11e4aef68073ba825ce1ec106222eb427c81fe72
Oleg <oleg.kholyavchuk@bk.ru>
2022-12-24 15:45:00 +0300

    BUT-98

--- src/test/java/ru/buttonone/constant/KinopoiskLocators.java
+++ src/test/java/ru/buttonone/constant/KinopoiskLocators.java
@@ -3,7 +3,6 @@ package ru.buttonone.constant;
 public class KinopoiskLocators {
     public static final String IMG_VK_COM_KINOPOISK = "//img[@alt='https://vk.com/kinopoisk']";
     public static final String IMG_YOUTUBE_USER_KINOPOISK = "//img[@alt='https://www.youtube.com/user/kinopoisk']";
-    public static final String INPUT_NAME_KP_QUERY = "//input[@name='kp_query']";
     public static final String KINOPOISK_HEAD_BUTTON = "//a[@href='https://hd.kinopoisk.ru/?source=kinopoisk_head_button']";
     public static final String JOBS_VACANCIES_SERVICES_KINOPOISK = "//a[@href='https://yandex.ru/jobs/vacancies?services=kinopoisk']";
 }
